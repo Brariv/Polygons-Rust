3a9744f62eb0ecd44b3d5c1e38284b0f700f12f9
Brariv <riv23088@uvg.edu.gt>
2025-07-12 17:45:24 -0600

    Polygon 3
    Triangulo Rojo orilla Blanca

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,11 @@ fn main() {
     let mut framebuffer = framebuffers::FrameBuffer::new(800, 600, Color::BLACK);
     
 
-    // (165, 380) (185, 360) (180, 330) (207, 345) (233, 330) (230, 360) (250, 380) (220, 385) (205, 410) (193, 383)
+    // (377, 249) (411, 197) (436, 249)
     let polygon_points = [
-        Vector2::new(165.0, 380.0),
-        Vector2::new(185.0, 360.0),
-        Vector2::new(180.0, 330.0),
-        Vector2::new(207.0, 345.0),
-        Vector2::new(233.0, 330.0),
-        Vector2::new(230.0, 360.0),
-        Vector2::new(250.0, 380.0),
-        Vector2::new(220.0, 385.0),
-        Vector2::new(205.0, 410.0),
-        Vector2::new(193.0, 383.0),
+        Vector2::new(377.0, 249.0),
+        Vector2::new(411.0, 197.0),
+        Vector2::new(436.0, 249.0)
     ];
 
     for i in 0..polygon_points.len() {
@@ -29,7 +22,7 @@ fn main() {
         line::line(&mut framebuffer, start, end, Color::WHITE);
     }
 
-    fill::fill(&mut framebuffer, &polygon_points, Color::WHITE);
+    fill::fill(&mut framebuffer, &polygon_points, Color::RED);
 
 
     framebuffer.draw_image("out.bmp");
